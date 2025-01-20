cf734e4a9a2b0b4d79c2e4a50809cea74a363a48
Lilac Gil-Ad <lilacgilad@mail.tau.ac.il>
2025-01-20 18:08:38 +0000

    added graph options to bonus

--- bonus.py
+++ bonus.py
@@ -4,11 +4,14 @@ from sklearn.datasets import load_iris
 from sklearn.cluster import KMeans
 from kmeans_pp import euclidean_distance
 
+import matplotlib.patheffects as pe
+
 def get_inertia(datapoints, centroids, labels):
     return sum([euclidean_distance(datapoint, centroids[labels[i]]) for i, datapoint in enumerate(datapoints)])
 
 def plot_graph(k_range, inertia_ls, opt_k):
     opt_k_point = np.array((opt_k, inertia_ls[opt_k-1]))
+    opt_k_point_2 = np.array((opt_k+1, inertia_ls[opt_k+1-1]))
     annotation_shift = np.array((0.3, 7))
 
     plt.cla()
@@ -17,10 +20,16 @@ def plot_graph(k_range, inertia_ls, opt_k):
     plt.xlabel("k")
     plt.ylabel("Inertia")
 
-    plt.plot(list(k_range), inertia_ls)
+    plt.plot((opt_k_point[0], opt_k_point_2[0]), (opt_k_point[1], opt_k_point_2[1]), color='lightblue', lw=10, 
+             solid_capstyle="round") # highlighting the range
 
-    plt.scatter(opt_k_point[0], opt_k_point[1], edgecolors="k", facecolors="none", s=200)
-    plt.annotate("Elbow Point", opt_k_point, opt_k_point+annotation_shift)
+    plt.plot(list(k_range), inertia_ls, color='royalblue')
+
+    # plt.scatter(opt_k_point[0], opt_k_point[1], edgecolors="k", facecolors="none", s=200) # circling p1
+    # plt.scatter(opt_k_point_2[0], opt_k_point_2[1], edgecolors="k", facecolors="none", s=200) # circling p2
+    # plt.annotate("Elbow Point", opt_k_point, opt_k_point+annotation_shift) # annotating p1 only
+    plt.annotate("Possible Elbow Points", (opt_k_point+opt_k_point_2)/2, 
+                 (opt_k_point+opt_k_point_2)/2+annotation_shift) # annotating both points
 
     plt.savefig("elbow.png")
 
